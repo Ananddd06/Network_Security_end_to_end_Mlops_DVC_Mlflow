447a211750b0d3919ca9b740cc4c93f74faba7c3
Ananddd06 <122982405+Ananddd06@users.noreply.github.com>
2025-07-09 15:04:50 +0530

    Added the data transformation pipeline

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -40,3 +40,16 @@ DATA_VALIDATION_INVALID_DIR = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME = "drift_report.html"
 DATA_VALIDATION_DRIFT_SUMMARY_FILE_NAME = "drift_summary.yaml" 
+
+"""
+Data transformation related constants
+"""
+DATA_TRANSFORMATION_DIR_NAME = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR : str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR : str = "transformed_object"
+
+DATA_TRANSFORMATION_IMPUTER_PARAMS : dict = {
+    "missing_values": np.nan,
+    "n-neighbors": 3,
+    "weights": "uniform",
+}
--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -80,3 +80,25 @@ class DataValidationConfigEntity:
             training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
             training_pipeline.DATA_VALIDATION_DRIFT_SUMMARY_FILE_NAME
         )
+    
+@dataclass
+class DataTransformationConfigEntity:
+    training_pipeline_config: TrainingPipelineConfigEntity
+
+    def __post_init__(self):
+        self.data_transformation_dir: str = os.path.join(
+            self.training_pipeline_config.artifact_dir,
+            training_pipeline.DATA_TRANSFORMATION_DIR_NAME
+        )
+        self.transformed_train_file_path: str = os.path.join(
+            self.data_transformation_dir,
+            training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DIR,
+            training_pipeline.TRAIN_FILE_NAME)
+        self.transformed_test_file_path: str = os.path.join(
+            self.data_transformation_dir,
+            training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DIR,
+            training_pipeline.TEST_FILE_NAME)
+        self.transformed_object_file_path: str = os.path.join(
+            self.data_transformation_dir,
+            training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DIR , 
+            training_pipeline.PREPROCESSING_OBJECT_FILE_NAME)
