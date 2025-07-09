eebc2220d96561c3b50a12d25047ab54c603d3b5
Ananddd06 <122982405+Ananddd06@users.noreply.github.com>
2025-07-09 11:01:33 +0530

    Added the schema file path

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -2,6 +2,7 @@ from networksecurity.entity.artifact_entity import DataValidationArtifact , Data
 from networksecurity.entity.config_entity import DataValidationConfig
 from networksecurity.exception.exception import CustomException
 from networksecurity.logger.customlogger import Custom_Logger
+from networksecurity.constants.training_pipeline import SCHEMA_FILE_PATH
 from scipy.stats import ks_2samp
 import pandas as pd
 import numpy as np
