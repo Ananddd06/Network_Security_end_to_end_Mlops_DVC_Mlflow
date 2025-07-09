17ae35f3f2d277b90e335c2e7053a641df25ad74
Ananddd06 <122982405+Ananddd06@users.noreply.github.com>
2025-07-09 17:38:59 +0530

    Added the estimator.py

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -6,6 +6,7 @@ from networksecurity.utils.ml_metric.classification_metric import get_classifica
 from networksecurity.entity.config_entity import ModelTrainerConfigEntity , DataTransformationConfigEntity
 from networksecurity.entity.artifact_entity import DataTransformationArtifactEntity , ModelTrainerArtifactEntity
 from networksecurity.utils.main_utils import save_obj , load_obj , load_numpy_array_data
+from networksecurity.utils.model_metric.estimator import NetworkModel
 
 class ModelTrainer:
     def __init__(self, model_trainer_config: ModelTrainerConfigEntity,
