7495fc963231accdb5cb88a38dcff429b627c00e
Ananddd06 <122982405+Ananddd06@users.noreply.github.com>
2025-07-09 10:40:34 +0530

    Added the training entity and config entity of the validation pipeline

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -19,9 +19,20 @@ MODEL_FILE_NAME: str = "model.pkl"
 Data ingestion related constants
 Reading directly from the local CSV file
 """
+
 ROOT_DIR = os.getcwd()  # Gets the current project root
 DATA_INGESTION_SOURCE_PATH: str = os.path.join(ROOT_DIR, "Network_Data", FILE_NAME)
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+"""
+Data validation related constants starts with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME : str = "data_validation"
+DATA_VALIDATION_VALID_DIR : str = "valid"
+DATA_VALIDATION_INVALID_DIR : str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR : str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME : str = "report.yaml"
