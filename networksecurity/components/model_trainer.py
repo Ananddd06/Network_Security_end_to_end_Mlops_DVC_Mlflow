48c19863e59f96f1a5c550be1d757aa6106bd865
Ananddd06 <122982405+Ananddd06@users.noreply.github.com>
2025-07-09 17:27:07 +0530

    Added the model traniner starting code

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -6,3 +6,22 @@ from networksecurity.utils.ml_metric.classification_metric import get_classifica
 from networksecurity.entity.config_entity import ModelTrainerConfigEntity , DataTransformationConfigEntity
 from networksecurity.entity.artifact_entity import DataTransformationArtifactEntity , ModelTrainerArtifactEntity
 from networksecurity.utils.main_utils import save_obj , load_obj , load_numpy_array_data
+
+class ModelTrainer:
+    def __init__(self, model_trainer_config: ModelTrainerConfigEntity,
+                 data_transformation_config: DataTransformationConfigEntity,
+                 data_transformation_artifact: DataTransformationArtifactEntity):
+        try:
+            self.model_trainer_config = model_trainer_config
+            self.data_transformation_config = data_transformation_config
+            self.data_transformation_artifact = data_transformation_artifact
+            self.logger = Custom_Logger().get_logger()
+            self.logger.info("Model Trainer initialized with configuration and artifacts.")
+        except Exception as e:
+            raise CustomException(e, sys) from e
+    
+    def initiate_model_trainer(self) -> ModelTrainerArtifactEntity:
+        try:
+           pass
+        except Exception as e:
+            raise CustomException(e, sys) 
