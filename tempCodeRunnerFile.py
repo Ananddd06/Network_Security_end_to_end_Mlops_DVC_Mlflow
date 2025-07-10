cb6f8fdf5689f18057997167777e439b7dd5db93
Ananddd06 <122982405+Ananddd06@users.noreply.github.com>
2025-07-10 13:09:50 +0530

    Added the yml file

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,27 +1,19 @@
 import sys
 import os
-
-# --- Debug Info ---
-print("--- Debug Info ---")
-print("Python Executable:", sys.executable)
-print("Python Path (sys.path):")
-for p in sys.path:
-    print(f"  - {p}")
-print("Current Working Directory (os.getcwd()):", os.getcwd())
-print("------------------")
-# --- End Debug Info ---
-
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_tranformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.logger.customlogger import Custom_Logger
 from networksecurity.exception.exception import CustomException
 from networksecurity.entity.config_entity import (
     DataIngestionConfigEntity,
     DataValidationConfigEntity,
+    DataTransformationConfigEntity,
     TrainingPipelineConfigEntity,
+    ModelTrainerConfigEntity
 )
-from networksecurity.entity.artifact_entity import DataIngestionArtifact
-
+from networksecurity.entity.artifact_entity import DataTransformationArtifact, ModelTrainerArtifact , DataIngestionArtifact
 logger = Custom_Logger().get_logger()
 
 if __name__ == "__main__":
@@ -46,9 +38,23 @@ if __name__ == "__main__":
         logger.info("✅ Data validation completed successfully.")
         print(f"📄 Data Validation Artifact: {data_validation_artifact}")
 
-    except CustomException as e:
-        logger.error(f"❌ Custom error in main pipeline: {e}")
-        raise e
+        # Data Transformation
+        data_transformation_config = DataTransformationConfigEntity(
+            training_pipeline_config=training_pipeline_config)
+        data_transformation = DataTransformation(
+            data_transformation_config=data_transformation_config,
+            data_validation_artifact=data_validation_artifact,
+        )
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logger.info("✅ Data transformation completed successfully.")
+        logger.info(f"📊 Data TransformationArtifact: {data_transformation_artifact}")
+
+        logger.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfigEntity(training_pipeline_config=training_pipeline_config)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logger.info("Model Training artifact created")
     except Exception as e:
         logger.error(f"❌ Unexpected error in main pipeline: {e}")
         raise CustomException(e, sys)
